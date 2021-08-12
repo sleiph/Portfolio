746ee4cc48ed06fbb596fa0bb964b0a087d98cda
Ricardo Gomes <sleiph@gmail.com>
2021-08-11 22:38:59 -0300

    minimo produto viavel

--- pages/index.js
+++ pages/index.js
@@ -10,23 +10,54 @@ const Titulo = styled.h1`
   color: ${({ theme }) => theme.colors.primaria};
 `
 
+const Corpo = styled.p`
+  max-width: 800px;
+`
+
 export default function Home() {
-  const artigos = [ 'aaa', 'bbb', 'ccc', 'ddd' ]
+  const artigos = [
+    {
+      nome:'aaa',
+      icone: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Video-Game-Controller-Icon-IDV-green.svg/2048px-Video-Game-Controller-Icon-IDV-green.svg.png',
+      conteudo: <h2>AAAaaa</h2>
+    },
+    {
+      nome:'bbb',
+      icone: 'https://seeklogo.com/images/W/windows-10-icon-logo-5BC5C69712-seeklogo.com.png',
+      conteudo: <h4>BBBbbb</h4>
+    },
+    {
+      nome:'ccc',
+      icone: 'https://img.icons8.com/fluency/452/microsoft-paint.png',
+      conteudo: <div>
+                  <h6>CCCccc</h6>
+                  <p>CCCccc</p>
+                </div>
+    },
+    {
+      nome:'ddd',
+      icone: 'https://cdn.iconscout.com/icon/free/png-512/microsoft-word-1411849-1194338.png',
+      conteudo: <img src='https://cupulatrovao.com.br/wp-content/uploads/2021/01/kono-dio-da.jpg' />
+    }
+  ]
 
   return (
     <Main>
-      <Titulo>OlaMundo</Titulo>
+      <Titulo>Ricardo Gomes</Titulo>
     
-      <p>Como você vai neste dia de hoje?</p>
+      <Corpo>Como você vai neste dia de hoje?</Corpo>
+
+      <Corpo>Não que eu esteja realmente interessado em como você está, eu só escrevi esse texto pra testar a aplicação mesmo.
+        A aplicação, aliás, vai ser o meu portfólio algum dia, mas se você já ficou interessado no meu trabalho só com essa página sem propósito, por que não entra no meu <a href='https://github.com/sleiph'>gitHub</a> e confere meus outros trabalhos?
+      </Corpo>
 
       {artigos.map((artigo) => {
         return (
           <Artigo
-            icone="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Video-Game-Controller-Icon-IDV-green.svg/2048px-Video-Game-Controller-Icon-IDV-green.svg.png"
-            titulo="Bloco de Notas"
-            conteudo={artigo}>
-            {artigo}
-          </Artigo>
+            icone={artigo.icone}
+            titulo={artigo.nome}
+            conteudo={artigo.conteudo}
+          />
         )
       })}
 
--- src/components/Artigo/index.js
+++ src/components/Artigo/index.js
@@ -2,32 +2,38 @@ import styled from 'styled-components';
 
 const Janela = styled.div`
   display: block;
-	max-width: 800px;
-	min-height: 20px;
+  max-width: 800px;
+  min-height: 24px;
   padding-bottom: 16px;
+
+  @media(min-width: 860px) {
+    position: absolute;
+
+  }
 `
 
 const Notas = styled.div`
   background: ${({ theme }) => theme.colors.janela};
   display: block;
-	min-height: 100px;
-	width: 100%;
-	padding: 4px;
-	-webkit-box-shadow: 5px 5px 5px 1px rgba(0,0,0,0.75);
-	-moz-box-shadow: 5px 5px 5px 1px rgba(0,0,0,0.75);
-	box-shadow: 3px 3px 5px 1px rgba(0,0,0,0.75);
-	border: 1.4px solid white;
+  min-height: 100px;
+  width: 100%;
+  padding: 4px;
+  -webkit-box-shadow: 5px 5px 5px 1px rgba(0,0,0,0.75);
+  -moz-box-shadow: 5px 5px 5px 1px rgba(0,0,0,0.75);
+  box-shadow: 3px 3px 5px 1px rgba(0,0,0,0.75);
+  border: 1.4px solid white;
 `
 
 const NotasBarra = styled.div`
   background: ${({ theme }) => theme.colors.windows};
   width: 100%;
-	height: 23px;
+  height: 24px;
+  margin-bottom: 2px;
 
   .icone {
     height: 100%;
 	  float: left;
-	  padding: 2px;
+	  padding: 3px;
   }
 
   img {
@@ -36,90 +42,80 @@ const NotasBarra = styled.div`
     height: 18px;
     width: 18px;
   }
-`
 
-const NotasBarraTitulo = styled.div`
-  color: white;
-	letter-spacing: .5px;
-	word-spacing: .5px;
-	padding-left: 5px;
-	float: left;
-	font-weight: 600;
-`
+  .titulo {
+	  color: white;
+    padding-left: 5px;
+	  padding-top: 2px;
+    float: left;
+  }
 
-const NotasBarraFechar = styled.div`
-  height: 100%;
-	display: block;
-	float: right;
-	padding-top: 3px;
-	padding-bottom: 3px;
-  padding-left:  5px;
-	padding-right: 5px;
-`
+  .fechar {
+  	height: 100%;
+  	display: block;
+  	float: right;
+  	padding-bottom: 3px;
+  	padding-left:  5px;
+  	padding-right: 5px;
 
-const NotasBarraFecharBotao = styled.div`
-  background: #BFBFBF;
-	height: 100%;
-	width: 19px;
-	border-right: 2px solid black;
-	border-bottom: 2px solid black;
-	border-left: 1px solid white;
-	border-top: 1px solid white;
-  font-size: 55%;
-	margin: auto;
-	text-align: center;
-`
-
-const NotasTools = styled.div`
-  width: 100%;
-	float: left;
-	display: inline-flex;
-	margin-left: 10px;
-	margin-bottom: 5px;
-`
-
-const NotasToolsTexto = styled.div`
-  padding-right: 15px;
-
-  span {
-    text-decoration: underline;
+    button {
+      background: ${({ theme }) => theme.colors.janela};
+      height: 20px;
+      width: 20px;
+      margin-top: 2px;
+      line-height: .9;
+      padding-left: 4px;
+      border-right: 2px solid black;
+      border-bottom: 2px solid black;
+      border-left: 1px solid white;
+      border-top: 1px solid white;
+    }
   }
 `
 
 const NotasContent = styled.div`
   background: white;
   height: 400px;
-	width: 100%;
-	clear: both;
-	border-radius: 1px;
-	border: 1.2px solid black;
-	overflow: auto;
-	padding: 5px;
+  width: 100%;
+  clear: both;
+  border-radius: 1px;
+  border: 1.2px solid black;
+  overflow: auto;
+  padding: 5px;
+
+  @media(min-width: 860px) {
+    min-width: 400px;
+  }
 `
 
 export default function Artigo({ icone, titulo, conteudo }) {
+  
+  const deleteDis = () => {
+    document.getElementById(titulo).remove()
+  }
+
   return (
-    <Janela>
-			<Notas>
-				<NotasBarra>
-					<div className="icone">
-						<img src={ icone } alt="icone do programa" />
-					</div>
-					<NotasBarraTitulo>{ titulo }</NotasBarraTitulo>
-						<NotasBarraFechar>
-							<NotasBarraFecharBotao>&#10006;</NotasBarraFecharBotao>
-						</NotasBarraFechar>
-				</NotasBarra>
-				<NotasTools>
-				 	<NotasToolsTexto><span>F</span>ile</NotasToolsTexto>
-				 	<NotasToolsTexto><span>E</span>dit</NotasToolsTexto>
-				 	<NotasToolsTexto><span>S</span>earch</NotasToolsTexto>
-				 	<NotasToolsTexto><span>H</span>elp</NotasToolsTexto>
-				</NotasTools>
-				<NotasContent>
-					{ conteudo }
-				</NotasContent>
-			</Notas>
+    <Janela id={ titulo }
+    style={{ top: (Math.floor(Math.random() * (40 - 10)) + 10) + 'vh',
+    left: (Math.floor(Math.random() * (60 - 0)) + 0) + 'vw' }}>
+      <Notas>
+        <NotasBarra>
+          <div className="icone">
+            <img src={ icone } alt="icone do programa" />
+          </div>
+          <div className="titulo">
+            { titulo }
+          </div>
+          <div className="fechar">
+            <button onClick={ deleteDis }>
+              &#10006;
+            </button>
+          </div>
+        </NotasBarra>
+        <NotasContent>
+          { conteudo }
+        </NotasContent>
+      </Notas>
     </Janela>
   )
 }
