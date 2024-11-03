b2497c7a7552455f0e7d7afa21a000a17971c32c
ricardO <sleiph@gmail.com>
2024-11-03 07:39:47 -0300

    separando tecnologia e framework nos projetos

--- src/components/Artigos/Projetos/index.js
+++ src/components/Artigos/Projetos/index.js
@@ -13,7 +13,8 @@ export default function Projetos() {
             <Pasta>
               <img src='https://66.media.tumblr.com/6ee194172c15584561b951ff258d9d1d/tumblr_odqaag4zd41vgs7gco3_75sq.png' alt={projeto.nome} />
               <p><b>{projeto.nome}</b></p>
-              <p>{projeto.tecnologia}</p>
+              <p>{projeto.tecnologia}/</p>
+              <p>{projeto.framework}</p>
             </Pasta>
           </a>
         )
